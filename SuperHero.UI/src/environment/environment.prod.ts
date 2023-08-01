bf1f426cb089da87c5244de1aa832c84e2915152
Dragon2000v <shmatko064@gmail.com>
2023-08-01 21:59:39 +0300

    finish
    finish

---
+++ SuperHero.UI/src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true, // Указывает, что это продакшн-среда
+    apiBaseUrl: 'https://api.example.com', // Базовый URL для API-запросов в продакшн
+    googleAnalyticsKey: 'YOUR_GOOGLE_ANALYTICS_KEY', // Ключ Google Analytics для отслеживания в продакшн
+    // Другие переменные среды, которые отличаются от разработки
+  };
+  
---
+++ SuperHeroAPI/SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        /*public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+
+        }*/
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
